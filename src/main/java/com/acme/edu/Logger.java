19ff0fed408ae8a24cf9bdc68535348a70d9ead5
DeSile <pl9lar@gmail.com>
2015-10-29 16:53:20 +0400

    add javadocs

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,16 +1,33 @@
 package com.acme.edu;
 
 public class Logger {
+
+    /**
+     * Используется для вывода переданного аргумента.
+     * @param message
+     */
     public static void log(int message) {
         print("primitive: " + message);
     }
 
+    /**
+     * Используется для вывода переданного аргумента.
+     * @param message
+     */
     public static void log(byte message) {
         print("primitive: " + message);
     }
 
+    /**
+     * Используется для вывода переданного аргумента.
+     * @param message
+     */
     public static void log(boolean message) { print("primitive: " + (message ? "true" : "false"));}
 
+    /**
+     * Используется для вывода переданного аргумента.
+     * @param message
+     */
     public static void log(char message) {print("char: " + message);}
 
     private static void print(String s){
